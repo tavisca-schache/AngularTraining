5f4983cf09434c6e4046f37ae2a9c5aeea8b3788
Saurabh Chache <schache@tavisca.com>
2020-05-13 02:25:36 +0530

    Added day 2 assignment

---
+++ Day2_AngularBeginning/myngapp/src/app/models/app.constants.ts
@@ -0,0 +1,9 @@
+export const Products = [
+    {ProductId:101, ProductName:'Laptop', Price:20000, Category:'ECT'},
+    {ProductId:102, ProductName:'Iron', Price:2000, Category:'ECL'},
+    {ProductId:103, ProductName:'Lays', Price:20, Category:'FOD'}
+];
+
+export const Categories = [
+    'ECT', 'ECL', 'FOD'
+];
